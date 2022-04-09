bce20f82d6922a18b15e28d85e38726209fcb77c
omer cohen <omerco@saitechs.io>
2022-04-09 21:23:53 +0300

    first 100% working relise

--- src/basic_flask/create_flask.py
+++ src/basic_flask/create_flask.py
@@ -15,16 +15,22 @@ def create():
     subprocess.run("python -m venv .venv")
     subprocess.run("mkdir Routes")
     subprocess.run("mkdir Modules")
-    subprocess.run("touch .env requirements.txt")
+    subprocess.run("touch .env requirements.txt .gitignore .flaskenv")
 
-    shutil.copy(f"{this_path}/creation_files/activate_this.py", f"{path}/.venv/scripts/activate_this.py")
-    shutil.copy(f"{this_path}/creation_files/main.py", f"{path}/main.py")
-    shutil.copy(f"{this_path}/creation_files/.gitignore", f"{path}/.gitignore")
-    shutil.copy(f"{this_path}/creation_files/.flaskenv", f"{path}/.flaskenv")
+    shutil.copy(f"{this_path}\\creation_files\\activate_this.py", f"{path}\\.venv\\scripts\\activate_this.py")
+    shutil.copy(f"{this_path}\\creation_files\\main.py", f"{path}\\main.py")
 
-    # Creates env file with random secret key
+    # Creates env file with random secret key 
     with open(f"{path}\\.env", "w") as env:
         env.write(f"SECRET_KEY={secrets.token_hex(48)}\n")
+
+    # Create .gitignore with some data
+    with open(f"{path}\\.gitignore", "w") as env:
+        env.write(".venv \n__pycache__\n")
+    
+    # Create .flaskenv with some data
+    with open(f"{path}\\.flaskenv", "w") as env:
+        env.write("FLASK_APP=main.py\nFLASK_ENV=development\nDEBUG=True\n")
 
     # active the new venv
     activator = f"{path}\\.venv\\scripts\\activate_this.py"
